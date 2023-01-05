32377784c64d90b475f953de2c048d25d7b766e3
AleksandrRumyantsev <alex_rumyncev@mail.ru>
2023-01-05 13:31:01 +0300

    Initial commit

---
+++ src/MyCalculator.java
@@ -0,0 +1,289 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyCalculator implements ActionListener {
+    double number, answer;
+    int calculation;
+    JFrame frame;//Creating object of JFrame class
+    //Creating objects of Components
+    JLabel label = new JLabel();
+    JTextField textField = new JTextField();
+    JButton buttonZero = new JButton("0");
+    JButton buttonOne = new JButton("1");
+    JButton buttonTwo = new JButton("2");
+    JButton buttonThree = new JButton("3");
+    JButton buttonFour = new JButton("4");
+    JButton buttonFive = new JButton("5");
+    JButton buttonSix = new JButton("6");
+    JButton buttonSeven = new JButton("7");
+    JButton buttonEight = new JButton("8");
+    JButton buttonNine = new JButton("9");
+    JButton buttonDot = new JButton(".");
+    JButton buttonClear = new JButton("C");
+    JButton buttonEqual = new JButton("=");
+    JButton buttonMul = new JButton("x");
+    JButton buttonDiv = new JButton("/");
+    JButton buttonPlus = new JButton("+");
+    JButton buttonMinus = new JButton("-");
+
+    MyCalculator()//Creating constructor of the class
+    {
+        prepareGUI();
+        addComponents();
+        addActionEvent();
+    }
+
+    public void prepareGUI() {
+        frame = new JFrame();
+        frame.setTitle("My Calculator");//Setting title of the JFrame
+        frame.setSize(300, 430);//Setting size
+        frame.getContentPane().setLayout(null);//Setting Layout
+        frame.setResizable(false);//Preventing window from resizing
+        frame.setLocationRelativeTo(null);//Setting location to the center of the screen
+        frame.setVisible(true);//Setting window's visibility
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Setting default close operation
+
+    }
+
+    public void addComponents() {
+        //Setting property of Label
+        label.setBounds(250, 0, 50, 50);
+        frame.add(label);
+
+        //Setting property of text field
+        textField.setBounds(10, 40, 270, 40);
+        textField.setFont(new Font("Arial", Font.BOLD, 20));
+        textField.setEditable(false);
+        textField.setHorizontalAlignment(SwingConstants.RIGHT);
+        frame.add(textField);
+
+
+        //Setting property of button 7
+        buttonSeven.setBounds(10, 170, 60, 40);
+        buttonSeven.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonSeven);
+
+        // Setting property of button 8
+        buttonEight.setBounds(80, 170, 60, 40);
+        buttonEight.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonEight);
+
+        //Setting property of button 9
+        buttonNine.setBounds(150, 170, 60, 40);
+        buttonNine.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonNine);
+
+        //Setting property of button 4
+        buttonFour.setBounds(10, 230, 60, 40);
+        buttonFour.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonFour);
+
+        //Setting property of button 5
+        buttonFive.setBounds(80, 230, 60, 40);
+        buttonFive.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonFive);
+
+        //Setting property of button 6
+        buttonSix.setBounds(150, 230, 60, 40);
+        buttonSix.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonSix);
+
+        //Setting property of button 1
+        buttonOne.setBounds(10, 290, 60, 40);
+        buttonOne.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonOne);
+
+        //Setting property of button 2
+        buttonTwo.setBounds(80, 290, 60, 40);
+        buttonTwo.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonTwo);
+
+        //Setting property of button 3
+        buttonThree.setBounds(150, 290, 60, 40);
+        buttonThree.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonThree);
+
+        //Setting property of dot button
+        buttonDot.setBounds(150, 350, 60, 40);
+        buttonDot.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonDot);
+
+        //Setting property of button 0
+        buttonZero.setBounds(10, 350, 130, 40);
+        buttonZero.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.add(buttonZero);
+
+        //Setting property of button =
+        buttonEqual.setBounds(220, 350, 60, 40);
+        buttonEqual.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonEqual.setBackground(new Color(239, 188, 2));
+        frame.add(buttonEqual);
+
+        //Setting property of button /
+        buttonDiv.setBounds(220, 110, 60, 40);
+        buttonDiv.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonDiv.setBackground(new Color(239, 188, 2));
+        frame.add(buttonDiv);
+
+
+        //Setting property of button X
+        buttonMul.setBounds(220, 230, 60, 40);
+        buttonMul.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonMul.setBackground(new Color(239, 188, 2));
+        frame.add(buttonMul);
+
+        //Setting property of button -
+        buttonMinus.setBounds(220, 170, 60, 40);
+        buttonMinus.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonMinus.setBackground(new Color(239, 188, 2));
+        frame.add(buttonMinus);
+        //Setting property of button +
+        buttonPlus.setBounds(220, 290, 60, 40);
+        buttonPlus.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonPlus.setBackground(new Color(239, 188, 2));
+        frame.add(buttonPlus);
+
+
+        //Setting property of clear button
+        buttonClear.setBounds(10, 110, 130, 40);
+        buttonClear.setFont(new Font("Arial", Font.BOLD, 12));
+        buttonClear.setBackground(Color.white);
+        buttonClear.setForeground(Color.red);
+        frame.add(buttonClear);
+
+    }
+
+    public void addActionEvent() {
+        //Registering ActionListener to buttons
+        buttonClear.addActionListener(this);
+        buttonDiv.addActionListener(this);
+        buttonMinus.addActionListener(this);
+        buttonSeven.addActionListener(this);
+        buttonEight.addActionListener(this);
+        buttonNine.addActionListener(this);
+        buttonMul.addActionListener(this);
+        buttonFour.addActionListener(this);
+        buttonFive.addActionListener(this);
+        buttonSix.addActionListener(this);
+        buttonPlus.addActionListener(this);
+        buttonOne.addActionListener(this);
+        buttonTwo.addActionListener(this);
+        buttonThree.addActionListener(this);
+        buttonEqual.addActionListener(this);
+        buttonZero.addActionListener(this);
+        buttonDot.addActionListener(this);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        Object source = e.getSource();
+
+        if (source == buttonClear) {
+            //Clearing texts of label and text field
+            label.setText("");
+            textField.setText("");
+
+        } else if (source == buttonZero) {
+            if (textField.getText().equals("0")) {
+                return;
+            } else {
+                textField.setText(textField.getText() + "0");
+            }
+        } else if (source == buttonOne) {
+            textField.setText(textField.getText() + "1");
+        } else if (source == buttonTwo) {
+            textField.setText(textField.getText() + "2");
+        } else if (source == buttonThree) {
+            textField.setText(textField.getText() + "3");
+        } else if (source == buttonFour) {
+            textField.setText(textField.getText() + "4");
+        } else if (source == buttonFive) {
+            textField.setText(textField.getText() + "5");
+        } else if (source == buttonSix) {
+            textField.setText(textField.getText() + "6");
+        } else if (source == buttonSeven) {
+            textField.setText(textField.getText() + "7");
+        } else if (source == buttonEight) {
+            textField.setText(textField.getText() + "8");
+        } else if (source == buttonNine) {
+            textField.setText(textField.getText() + "9");
+        } else if (source == buttonDot) {
+            if (textField.getText().contains(".")) {
+                return;
+            } else {
+                textField.setText(textField.getText() + ".");
+            }
+
+        } else if (source == buttonPlus) {
+            String str = textField.getText();
+            number = Double.parseDouble(textField.getText());
+            textField.setText("");
+            label.setText(str + "+");
+            calculation = 1;
+        } else if (source == buttonMinus) {
+            String str = textField.getText();
+            number = Double.parseDouble(textField.getText());
+            textField.setText("");
+            label.setText(str + "-");
+            calculation = 2;
+        } else if (source == buttonMul) {
+            String str = textField.getText();
+            number = Double.parseDouble(textField.getText());
+            textField.setText("");
+            label.setText(str + "X");
+            calculation = 3;
+        } else if (source == buttonDiv) {
+            String str = textField.getText();
+            number = Double.parseDouble(textField.getText());
+            textField.setText("");
+            label.setText(str + "/");
+            calculation = 4;
+
+        } else if (source == buttonEqual) {
+            //Setting functionality for equal(=) button
+            switch (calculation) {
+                case 1:
+                    answer = number + Double.parseDouble(textField.getText());
+                    if (Double.toString(answer).endsWith(".0")) {
+                        textField.setText(Double.toString(answer).replace(".0", ""));
+                    } else {
+                        textField.setText(Double.toString(answer));
+                    }
+                    label.setText("");
+                    break;
+                case 2:
+                    answer = number - Double.parseDouble(textField.getText());
+                    if (Double.toString(answer).endsWith(".0")) {
+                        textField.setText(Double.toString(answer).replace(".0", ""));
+                    } else {
+                        textField.setText(Double.toString(answer));
+                    }
+                    label.setText("");
+                    break;
+                case 3:
+                    answer = number * Double.parseDouble(textField.getText());
+                    if (Double.toString(answer).endsWith(".0")) {
+                        textField.setText(Double.toString(answer).replace(".0", ""));
+                    } else {
+                        textField.setText(Double.toString(answer));
+                    }
+                    label.setText("");
+                    break;
+                case 4:
+                    answer = number / Double.parseDouble(textField.getText());
+                    if (Double.toString(answer).endsWith(".0")) {
+                        textField.setText(Double.toString(answer).replace(".0", ""));
+                    } else {
+                        textField.setText(Double.toString(answer));
+                    }
+                    label.setText("");
+                    break;
+            }
+        }
+    }
+}
